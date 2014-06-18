4e461b35a83a9937101e45065b7a4775d870f4dc
Roman <RomzesRover@gmail.com>
2014-06-18 21:07:22 +0400

    add pulse orange for full screen as an progress bar

--- src/com/BBsRs/hspaptweaker/TaskerActivity.java
+++ src/com/BBsRs/hspaptweaker/TaskerActivity.java
@@ -5,7 +5,10 @@ import android.app.ActivityManager;
 import android.app.ActivityManager.RunningServiceInfo;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.preference.PreferenceManager;
 import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.Menu;
@@ -21,13 +24,17 @@ import android.widget.TextView;
 public class TaskerActivity extends Activity {
 	
 																		//for animation
+	SharedPreferences sharedPrefs;
 	private Context mContext;
+	public boolean animationPeriod = false;
 	private static final int ANIMATION_DURATION = 300;
+	private static final int ANIMATION_DURATION_TWO = 1300;
 	private float mFullScreenScale;
 	private boolean isStarted = false;
 	
 	private TextView textButton;										//text button
     private ImageView mBackgroundShape;									//orange oval shape wrap text
+    private ImageView mBackgroundShape2;									//orange oval shape wrap text
     
     Menu mainMenu = null;												//global variable of menu 
 
@@ -37,6 +44,8 @@ public class TaskerActivity extends Activity {
 	    super.onCreate(savedInstanceState);
 	    
 	    																//init
+	    sharedPrefs = PreferenceManager
+				.getDefaultSharedPreferences(getApplicationContext());
 	    getWindow().requestFeature(Window.FEATURE_ACTION_BAR);			//show action bar
         getActionBar().show();
         getActionBar().setTitle(R.string.app_name);						//set title
@@ -44,6 +53,7 @@ public class TaskerActivity extends Activity {
         
 	    super.setContentView(R.layout.activity_tasker);
 	    mBackgroundShape = (ImageView) findViewById(R.id.bg);
+	    mBackgroundShape2 = (ImageView) findViewById(R.id.bg2);
         textButton = (TextView)findViewById(R.id.textButton);
         mContext = this;
         
@@ -91,6 +101,56 @@ public class TaskerActivity extends Activity {
 		return true;
 	}
 	
+	private timer CountDownTimer; 										// for timer4 Animation
+
+	public class timer extends CountDownTimer {
+
+		public timer(long millisInFuture, long countDownInterval) {
+			super(millisInFuture, countDownInterval);
+		}
+
+		@Override
+		public void onFinish() {
+			
+			if (!animationPeriod) {										//check what queue for animation
+	        mBackgroundShape2.animate()
+	        .alpha(1.0f)												//make second visible
+	        .setInterpolator(new AccelerateDecelerateInterpolator())
+	        .setDuration(ANIMATION_DURATION_TWO);
+	        
+	        mBackgroundShape.animate()
+	        .alpha(0.0f)												//make  first invisible
+	        .setInterpolator(new AccelerateDecelerateInterpolator())
+	        .setDuration(ANIMATION_DURATION_TWO);
+			} else {
+				mBackgroundShape.animate()
+		        .alpha(1.0f)											//make first visible
+		        .setInterpolator(new AccelerateDecelerateInterpolator())
+		        .setDuration(ANIMATION_DURATION_TWO);
+		        
+		        mBackgroundShape2.animate()
+		        .alpha(0.0f)											//make second invisible
+		        .setInterpolator(new AccelerateDecelerateInterpolator())
+		        .setDuration(ANIMATION_DURATION_TWO);
+			}
+	        
+	        animationPeriod = ! animationPeriod;
+			startCicleAnimation();										//cicle animation
+		}
+
+		@Override
+		public void onTick(long arg0) {
+		}
+	}
+	
+	public void startCicleAnimation() {
+		if (isStarted) {												//if service is killed stop timer in new thread recursive
+			CountDownTimer = new timer(ANIMATION_DURATION_TWO, ANIMATION_DURATION_TWO); 					// timer to 1 sec def value
+			
+			CountDownTimer.start(); 									// start timer
+		}
+	}
+	
 
     private void onTaskServiceOn() {
     	isStarted = true;
@@ -109,7 +169,15 @@ public class TaskerActivity extends Activity {
                 .scaleY(mFullScreenScale)
                 .setInterpolator(new AccelerateDecelerateInterpolator())
                 .setDuration(ANIMATION_DURATION);
+        
+        mBackgroundShape2.animate()										//make similar form to second oval
+        .scaleX(mFullScreenScale)
+        .scaleY(mFullScreenScale)
+        .setInterpolator(new AccelerateDecelerateInterpolator())
+        .setDuration(ANIMATION_DURATION);
         textButton.setTextColor(mContext.getResources().getColor(R.color.notWhite));
+        
+        startCicleAnimation();											//start nice animation
     }
 
     private void onTaskServiceOff() {
@@ -126,6 +194,12 @@ public class TaskerActivity extends Activity {
                 .scaleY(1)
                 .setInterpolator(new OvershootInterpolator())
                 .setDuration(ANIMATION_DURATION);
+        
+        mBackgroundShape2.animate()										//make similar form to second oval
+        .scaleX(1)
+        .scaleY(1)
+        .setInterpolator(new OvershootInterpolator())
+        .setDuration(ANIMATION_DURATION);
         textButton.setTextColor(mContext.getResources().getColor(R.color.notBlack));
     }
     
