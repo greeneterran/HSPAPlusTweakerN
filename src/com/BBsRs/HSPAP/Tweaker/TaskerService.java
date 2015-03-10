159200c6fbfda03912d6b1c956e28741de8dff14
Roman <RomzesRover@gmail.com>
2015-03-11 02:44:30 +0500

    add support github servers

--- src/com/BBsRs/HSPAP/Tweaker/TaskerService.java
+++ src/com/BBsRs/HSPAP/Tweaker/TaskerService.java
@@ -102,7 +102,7 @@ public class TaskerService extends Service {
 			Thread thr = new Thread(new Runnable() {
 				public void run() {
 					try {
-						Document doc = Jsoup.connect("http://brothers-rovers.3dn.ru/HPlusTweaker/"+sPref.getString("fileSize", getResources().getString(R.string.defaultFileSize))+".txt").get();
+						Document doc = Jsoup.connect(sPref.getString("downloadSource", "http://brothers-rovers.3dn.ru/HPlusTweaker/")+sPref.getString("fileSize", getResources().getString(R.string.defaultFileSize))+".txt").get();
 						Intent i = new Intent("DOWNLOAD_UPDATED");
 						i.putExtra("lastLogStroke",getResources().getString(R.string.defaultLogMessageSuccess)+" "+sPref.getString("fileSize", getResources().getString(R.string.defaultFileSize))+" "+getResources().getString(R.string.defaultLogMessageByte));
 						i.putExtra("errorOccurred", false);
